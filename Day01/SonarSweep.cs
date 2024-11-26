061844b979bcfaea40cc175a208518a1dfc56bdd
silentDoc <ramon78t@yahoo.es>
2024-11-26 17:04:49 +0100

    Day 1 - Part 2 Completed. Sonar sweeping with windows

--- Day01/SonarSweep.cs
+++ Day01/SonarSweep.cs
@@ -8,7 +8,14 @@ namespace AoC21.Day01
         public void ParseInput(List<string> input)
             => readings = input.Select(int.Parse).ToList();
 
+        int SolvePart2()
+        {
+            var windows = readings.Windowed(3).Select(x => x.Sum()).ToList();   // Common - Listutils
+            return Enumerable.Range(1, windows.Count() - 1).Select(i => windows[i] - windows[i - 1]).Count(r => r > 0);
+        }
+
         public int Solve(int part = 1)
-            => Enumerable.Range(1, readings.Count() - 1).Select(i => readings[i] - readings[i - 1]).Count(r => r > 0);
+            => part == 1 ? Enumerable.Range(1, readings.Count() - 1).Select(i => readings[i] - readings[i - 1]).Count(r => r > 0)
+                         : SolvePart2(); 
     }
 }
