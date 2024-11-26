203d38b4b37638aa3ec05e991f27d74b75f6e5df
silentDoc <ramon78t@yahoo.es>
2024-11-26 16:49:50 +0100

    Day 1 - Readme, main and inputs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,36 @@
-﻿namespace AoC21
+﻿using System.Diagnostics;
+
+namespace AoC20
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int day = 1;
+            int part = 1;
+            bool test = !false;
+            int testNum = 0;
+
+            string input = "./Input/day" + day.ToString("00");
+            input += (test) ? "_test" + (testNum > 0 ? testNum.ToString() : "") + ".txt" : ".txt";
+
+            Console.WriteLine("AoC 2021 - Day {0} , Part {1} - Test Data {2}", day, part, test);
+            Stopwatch st = new();
+            st.Start();
+            string result = day switch
+            {
+                1 => day1(input, part),
+                _ => throw new ArgumentException("Wrong day number - unimplemented")
+            };
+            st.Stop();
+            Console.WriteLine("Result : {0}", result);
+            Console.WriteLine("Elapsed : {0}", st.Elapsed.TotalSeconds);
+        }
+
+        static string day1(string input, int part)
+        {
+            var lines = File.ReadAllLines(input).ToList();
+            return "";
         }
     }
 }
