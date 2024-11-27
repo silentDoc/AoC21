aa2a28b402cb2cd608e9db5a112b528d305135b1
silentDoc <ramon78t@yahoo.es>
2024-11-27 09:14:57 +0100

    Day 6 - Readme, main and inputs

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ namespace AoC21
     {
         static void Main(string[] args)
         {
-            int day = 5;
-            int part = 2
-            bool test = false;
+            int day = 6;
+            int part = 1;
+            bool test = !false;
             int testNum = 0;
 
             string input = "./Input/day" + day.ToString("00");
@@ -24,6 +24,7 @@ namespace AoC21
                 3 => day3(input, part),
                 4 => day4(input, part),
                 5 => day5(input, part),
+                6 => day6(input, part),
                 _ => throw new ArgumentException("Wrong day number - unimplemented")
             };
             st.Stop();
@@ -69,6 +70,14 @@ namespace AoC21
             Day05.Liner liner = new();
             liner.ParseInput(lines);
             return liner.Solve(part).ToString();
+        }
+
+        static string day6(string input, int part)
+        {
+            var lines = File.ReadAllLines(input).ToList();
+            //Day05.Liner liner = new();
+            //liner.ParseInput(lines);
+            return "";//liner.Solve(part).ToString();
         }
     }
 }
