13dff644eac063d08b74130791c924407b9bda75
silentDoc <ramon78t@yahoo.es>
2024-11-27 13:02:50 +0100

    Day 7 - Part 2 Completed. Bruteforced with memoization

--- Day07/CrabAligner.cs
+++ Day07/CrabAligner.cs
@@ -2,17 +2,35 @@
 {
     internal class CrabAligner
     {
+        Dictionary<int, int> Lookup = new();
         List<int> crabs = new();
         public void ParseInput(List<string> input)
             => crabs = input[0].Split(",").Select(int.Parse).ToList();
 
         int Cost(int pos)
             => crabs.Select(x => Math.Abs(x - pos)).Sum();
+        
+        int CostInc(int pos)
+        {
+            if (!Lookup.ContainsKey(pos))
+            {
+                int cost = 0;
+                for (int i = 0; i <= pos; i++)
+                    cost += i;
+                Lookup[pos] = cost;
+            }
 
-        int AlignCrabs()
-            => Enumerable.Range(crabs.Min(), crabs.Max() - crabs.Min()).ToList().Min(x => Cost(x));
+            return Lookup[pos];
+        }
 
-        public int Solve(int part = 0)
-            => AlignCrabs();
+        int IncrementalCost(int pos)
+            => crabs.Select(x => CostInc(Math.Abs(x - pos))).Sum();
+
+        int AlignCrabs(int part = 1)
+            => part == 1 ? Enumerable.Range(crabs.Min(), crabs.Max() - crabs.Min()).ToList().Min(x => Cost(x))
+                         : Enumerable.Range(crabs.Min(), crabs.Max() - crabs.Min()).ToList().Min(x => IncrementalCost(x));
+
+        public int Solve(int part = 1)
+            => AlignCrabs(part);
     }
 }
