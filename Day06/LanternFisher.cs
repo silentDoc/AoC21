c461d76a688048f09518a51b87fc53313538cdba
silentDoc <ramon78t@yahoo.es>
2024-11-27 12:07:25 +0100

    Day 6 - Part 2 Completed. Flipped the problem over.

--- Day06/LanternFisher.cs
+++ Day06/LanternFisher.cs
@@ -27,7 +27,43 @@
             return state.Count();
         }
 
-        public int Solve(int part = 0)
-            => SolvePart1(80);
+        long SolveByDays(int rounds)
+        {
+            var daysToNums = new Dictionary<int, long>();
+
+            daysToNums[0] = 0;
+            daysToNums[1] = 0;
+            daysToNums[2] = 0;
+            daysToNums[3] = 0;
+            daysToNums[4] = 0;
+            daysToNums[5] = 0;
+            daysToNums[6] = 0;
+            daysToNums[7] = 0;
+            daysToNums[8] = 0;
+            daysToNums[9] = 0;
+
+            foreach (var num in state)
+                daysToNums[num]++;
+
+            for (int round = 0; round < rounds; round++)
+            {
+                daysToNums[7] += daysToNums[0];
+                daysToNums[9] += daysToNums[0];
+
+                for(int i=1;i<=9;i++)
+                    daysToNums[i-1] = daysToNums[i];
+
+                daysToNums[9] = 0;
+            }
+
+            long retVal = 0;
+            foreach(var key in daysToNums.Keys)
+                retVal += daysToNums[key];
+            
+            return retVal;
+        }
+
+        public long Solve(int part = 0)
+            => part == 1? SolvePart1(80) : SolveByDays(256);
     }
 }
