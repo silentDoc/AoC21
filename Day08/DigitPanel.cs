8701b8c701ff5ec27815c670e537a7a41f96c5f5
silentDoc <ramon78t@yahoo.es>
2024-11-28 23:04:54 +0100

    Day 8 - Part 2 Completed. Sorting input strings was key

--- Day08/DigitPanel.cs
+++ Day08/DigitPanel.cs
@@ -1,4 +1,6 @@
-﻿namespace AoC21.Day08
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AoC21.Day08
 {
     class SignalTest
     { 
@@ -8,8 +10,42 @@
         public SignalTest(string input)
         { 
             var parts = input.Split(" | ");
-            inputSignals = parts[0].Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).ToList();
-            outputValues = parts[1].Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).ToList();
+            var temp = parts[0].Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).ToList();
+            inputSignals = temp.Select(x => string.Concat(x.OrderBy(x => x))).ToList();
+            temp = parts[1].Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).ToList();
+            outputValues = temp.Select(x => string.Concat(x.OrderBy(x => x))).ToList();
+        }
+
+        public int Value()
+        {
+            // Work out the connections
+            var dict  = new Dictionary<string, int>();
+            var one   = inputSignals.Single(x => x.Length == 2);
+            dict[one] = 1;
+            var seven = inputSignals.Single(x => x.Length == 3);
+            dict[seven] = 7;
+            var four  = inputSignals.Single(x => x.Length == 4);
+            dict[four] = 4;
+            var eight = inputSignals.Single(x => x.Length == 7);
+            dict[eight] = 8;
+            var three = inputSignals.Single(x => x.Length == 5 && x.Intersect(seven).Count() == 3); 
+            dict[three] = 3;
+            var two   = inputSignals.Single(x => x.Length == 5 && x.Intersect(four).Count() == 2);
+            dict[two] = 2;
+            var nine = inputSignals.Single(x => x.Length == 6 && x.Intersect(three).Count() == 5);
+            dict[nine] = 9;
+            var five = inputSignals.Single(x => x.Length == 5 && !dict.ContainsKey(x));
+            dict[five] = 5;
+            var six   = inputSignals.Single(x => x.Length == 6 && x.Intersect(one).Count() != 2);
+            dict[six] = 6;
+            var zero  = inputSignals.Single(x => !dict.ContainsKey(x));
+            dict[zero] = 0;
+
+            string retStr = "";
+            foreach(var digit in outputValues)
+                retStr += dict[digit].ToString();
+
+            return int.Parse(retStr);
         }
     }
 
@@ -38,6 +74,6 @@
 
 
         public int Solve(int part = 1)
-            => SolvePart1();
+            => part == 1 ? SolvePart1() : signals.Sum(x => x.Value());
     }
 }
