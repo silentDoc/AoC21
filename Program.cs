1156aa69fec4103f231f4e664c179593369a4fe2
silentDoc <ramon78t@yahoo.es>
2024-12-06 23:03:00 +0100

    Day 21 - Readme, main and input

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ namespace AoC21
     {
         static void Main(string[] args)
         {
-            int day = 20;
-            int part = 2;
-            bool test = false;
+            int day = 21;
+            int part = 1;
+            bool test = !false;
             int testNum = 0;
 
             string input = "./Input/day" + day.ToString("00");
@@ -39,6 +39,7 @@ namespace AoC21
                 18 => day18(input, part),
                 19 => day19(input, part),
                 20 => day20(input, part),
+                21 => day21(input, part),
                 _ => throw new ArgumentException("Wrong day number - unimplemented")
             };
             st.Stop();
@@ -204,6 +205,14 @@ namespace AoC21
             Day20.ImageEnhancer daySolver = new();
             daySolver.ParseInput(lines);
             return  daySolver.Solve(part).ToString();
+        }
+
+        static string day21(string input, int part)
+        {
+            var lines = File.ReadAllLines(input).ToList();
+            //Day20.DiceRoller daySolver = new();
+            //daySolver.ParseInput(lines);
+            return ""; // daySolver.Solve(part).ToString();
         }
     }
 }
