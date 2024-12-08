f59d22260bb1da5c4ac36458b0ce99d563e637cc
silentDoc <ramon78t@yahoo.es>
2024-12-08 20:58:39 +0100

    Day 21 - Part 2 Completed. Learnt that class and records handle hashcodes pretty differently

--- Day21/DiceRoller.cs
+++ Day21/DiceRoller.cs
@@ -2,9 +2,11 @@
 {
     static class DiracDice
     {
-        public static IEnumerable<int> ThrowDice() => new[] { 1, 2, 3 }
-                                                     .SelectMany(i => new[] { 1, 2, 3 }, (i, j) => new { i, j })
-                                                     .SelectMany(pair => new[] { 1, 2, 3 }, (pair, k) => pair.i + pair.j + k);
+        public static IEnumerable<int> ThrowDice() 
+            => from i in new[] { 1, 2, 3 }
+               from j in new[] { 1, 2, 3 }
+               from k in new[] { 1, 2, 3 }
+               select i + j + k;
     }
 
     static class D100Dice
@@ -16,17 +18,8 @@
         }
     }
 
-    class Player
+    record Player(int Pos, int Score)
     {
-        public int Pos = 0;
-        public int Score = 0;
-
-        public Player(int pos, int score)
-        { 
-            Pos = pos;
-            Score = score;
-        }
-
         public Player Move(int steps)
         {
             var newPos = (Pos - 1 + steps) % 10 + 1;
@@ -34,11 +27,11 @@
         }
     }
 
-
     internal class DiceRoller
     {
         Player player1;
         Player player2;
+        new Dictionary<(Player, Player), (long, long)> memoize = new(); // for part 2
 
         public void ParseInput(List<string> input)
         {
@@ -73,7 +66,33 @@
             return loserScore * rounds * 3; // Each round has 3 rolls
         }
 
-        public int Solve(int part = 1)
-            => PlayGame();
+        (long activeWins, long otherWins) WinCounter( (Player active, Player other) players)
+        {
+            if (players.other.Score >= 21)
+                return (0, 1);
+
+            if (!memoize.ContainsKey(players))
+            {
+                var (activeWins, otherWins) = (0L, 0L);
+                foreach (var steps in DiracDice.ThrowDice())
+                {
+                    // We recurse switching active and other - so we address it at the sum below
+                    var wins = WinCounter((players.other, players.active.Move(steps)));
+                    activeWins += wins.otherWins;   
+                    otherWins += wins.activeWins;
+                }
+                memoize[players] = (activeWins, otherWins);
+            }
+            return memoize[players];
+        }
+
+        long PlayGameDirac()
+        {
+            var gamesWon = WinCounter((player1, player2));
+            return Math.Max(gamesWon.activeWins, gamesWon.otherWins);
+        }
+
+        public long Solve(int part = 1)
+            => part == 1 ? PlayGame() : PlayGameDirac();
     }
 }
