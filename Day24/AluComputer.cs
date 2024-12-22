b63bdc2033f6264bd78cf86337eabcfc1d6970da
silentDoc <ramon78t@yahoo.es>
2024-12-22 23:21:20 +0100

    Day 24 - Part 1 Completed. Needed to look for hints online, I suck at dissassembly. Tough one :(

--- Day24/AluComputer.cs
+++ Day24/AluComputer.cs
@@ -1,15 +1,11 @@
-﻿using AoC21.Common;
-using System.Net;
-
-namespace AoC21.Day24
+﻿namespace AoC21.Day24
 {
     record Instruction(string command, string param1, string param2);
 
     internal class AluComputer
     {
         List<Instruction> sourceCode = new();
-        Dictionary<string, int> vars = new();
-        Queue<int> inputs = new();
+
         Instruction ParseLine(string line)
         {
             var g = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
@@ -19,70 +15,52 @@ namespace AoC21.Day24
         public void ParseInput(List<string> lines)
            => lines.ForEach(x => sourceCode.Add(ParseLine(x)));
 
-        // This will not work, obviously - left here for the laughs
-        void ExecuteIns(Instruction ins)
+        long HackAlu()
         {
-            int param2Value = 0;
-            if (ins.param2 != "")
-                param2Value = (char.IsDigit(ins.param2[0]) || ins.param2[0] == '-') ? int.Parse(ins.param2) : vars[ins.param2];
-          
-            switch (ins.command)
+            var digits = Enumerable.Range(1, 9).ToList();
+
+            // Divide the instruction list into chunks of 18 lines
+            var blocks = sourceCode.Chunk(18).ToList();
+            var stack = new Stack<int>();
+
+            Dictionary<int, int> maxDigits = new();
+
+            foreach (var k in Enumerable.Range(0, 14))
+                maxDigits[k] = -1;
+
+            for (var j = 0; j < 14; j++)
             {
-                case "inp":
-                    vars[ins.param1] = inputs.Dequeue();
-                    break;
-                case "add":
-                    vars[ins.param1] = vars[ins.param1] + param2Value;
-                    break;
-                case "mul":
-                    vars[ins.param1] = vars[ins.param1] * param2Value;
-                    break;
-                case "div":
-                    vars[ins.param1] = vars[ins.param1] / param2Value;
-                    break;
-                case "mod":
-                    vars[ins.param1] = MathHelper.Modulo(vars[ins.param1],param2Value);
-                    break;
-                case "eql":
-                    vars[ins.param1] = (vars[ins.param1] == param2Value) ? 1 : 0;   
-                    break;
+                if (blocks[j][4].param2 == "1")     // Chunk with "div z 1" instead of "div z 26"
+                    stack.Push(j);
+                else
+                {
+                    var i = stack.Pop();
+                    var increment = int.Parse(blocks[j][5].param2) + int.Parse(blocks[i][15].param2);
+
+                    foreach (var a in digits)
+                    {
+                        var b = a + increment;
+
+                        if (digits.Contains(b))
+                            if (a > maxDigits[i])
+                            {
+                                maxDigits[i] = a;
+                                maxDigits[j] = b;
+                            }
+                    }
+
+                }
             }
+
+            long res = 0;
+
+            foreach (var k in Enumerable.Range(0, 14))
+                res = res * 10 + maxDigits[k];
+
+            return res;
         }
-
-        int RunProgram()
-        {
-            foreach (var ins in sourceCode)
-                ExecuteIns(ins);
-            return vars["z"];
-        }
-
-        public long SolvePart1()
-        {
-            for (long i = 99999999999999; i >= 11111111111111; i--)
-            {
-                vars = new();
-                vars["w"] = 0;
-                vars["x"] = 0;
-                vars["y"] = 0;
-                vars["z"] = 0;
-
-                inputs = new();
-                var str = i.ToString();
-                if (str.IndexOf('0') != -1)
-                    continue;
-
-                var digits = str.Select(x => int.Parse(x.ToString())).ToList();
-                foreach (var digit in digits)
-                    inputs.Enqueue(digit);
-
-                if (RunProgram() == 0)
-                    return i;
-            }
-            return 0;
-        }
-
 
         public long Solve(int part = 1)
-            => SolvePart1();
+            => HackAlu();
     }
 }
